8423df881dbd17f8c9357bc70c11dbccbeef418c
Kalana Dananjaya <kalanadananjaya855@gmail.com>
2023-10-31 22:46:33 +0530

    add Eureka server to package and user servers

--- BackEnd/Package/Package/src/main/java/lk/ijse/PackageApplication.java
+++ BackEnd/Package/Package/src/main/java/lk/ijse/PackageApplication.java
@@ -3,9 +3,11 @@ package lk.ijse;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class PackageApplication {
 
     public static void main(String[] args) {
