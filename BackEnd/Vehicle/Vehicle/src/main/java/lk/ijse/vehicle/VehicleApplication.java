33e4668f8583d1479ee6c5e830976b21069538a1
Kalana Dananjaya <kalanadananjaya855@gmail.com>
2023-10-31 23:00:20 +0530

    Gateway Created

--- BackEnd/Vehicle/Vehicle/src/main/java/lk/ijse/vehicle/VehicleApplication.java
+++ BackEnd/Vehicle/Vehicle/src/main/java/lk/ijse/vehicle/VehicleApplication.java
@@ -3,9 +3,11 @@ package lk.ijse.vehicle;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class VehicleApplication {
 
     public static void main(String[] args) {
